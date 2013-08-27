669388bf2a0dd6b8adda1a9ef44cbcb619488fdf
vlauterio1 <vlauterio1@gmail.com>
2013-08-27 07:38:21 -0700

    Used SortedDictionary for guarantee Keys order

--- 1-5 StringCompression/Program.cs
+++ 1-5 StringCompression/Program.cs
@@ -13,11 +13,11 @@ namespace _1_5_StringCompression
 
         static void Main(string[] args)
         {
-        	Console.WriteLine("abbcccdddd".Compress() == "a1b2c3d4");
-        	Console.WriteLine("a".Compress() == "a");
-        	Console.WriteLine("aabb".Compress() == "a2b2");
-        	Console.WriteLine("abbbcccbbccccccccddddaabcd".Compress() == "a1b3c3b2c8d4a2b1c1d1");
-        	Console.WriteLine("aA".Compress() == "aA");
+        	Console.WriteLine(("abbcccdddd".Compress() == "a1b2c3d4") == true);
+        	Console.WriteLine(("a".Compress() == "a") == true);
+        	Console.WriteLine(("aabb".Compress() == "a2b2") == true);
+        	Console.WriteLine(("abbbcccbbccccccccddddaabcd".Compress() == "a1b3c3b2c8d4a2b1c1d1") == true);
+            Console.WriteLine(("aA".Compress() == "aA") == true);
         }
     }
 
@@ -25,14 +25,14 @@ namespace _1_5_StringCompression
     {
     	public static string Compress(this string  s)
     	{
-    		IDictionary<int, int> duplicates = new Dictionary<int, int>();
+            IDictionary<int, int> duplicates = new SortedDictionary<int, int>();
     		int pos = -1;
 
     		for (int i = 0; i < s.Length; i++)
     		{
     			if (pos == -1 || s[pos] != s[i])
     			{
-    				duplicates.Add(i, 1);
+                    duplicates.Add(i, 1);
 
     				pos = i;
     			}
@@ -44,7 +44,7 @@ namespace _1_5_StringCompression
 
     		StringBuilder sb = new StringBuilder();
 
-    		foreach(KeyValuePair<int, int> d in duplicates)
+    		foreach (KeyValuePair<int, int> d in duplicates)
     		{
     			sb.Append(s[d.Key] + d.Value.ToString());
     		}
